b43ffabeef7732ac46792a8c017f6da8c0c441c9
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2024-12-20 02:59:12 +0530

    Update Task-B-1.js

--- Task-B-1.js
+++ Task-B-1.js
@@ -3,7 +3,7 @@ let keyPressed = {};
 let indx = 0;
 
 document.addEventListener("keydown", (event) => (keyPressed[event.key] = true));
-document.addEventListener("keyup", (event) => (keyPressed[event.key] = false));
+document.addEventListener("keyup", (event) => (keyPressed[event.key] = false)); // to create the effect of holding keys
 
 for (let item of checkboxes) {
   item.addEventListener("change", () => {
@@ -11,13 +11,13 @@ for (let item of checkboxes) {
     let i = checkboxesArray.indexOf(item);
 
     if (keyPressed["Shift"] && item.checked) checker(i);
-    if (item.checked) indx = i;
+    if (item.checked) indx = i; // starting index taken only when checked
   });
 }
 
 function checker(indx2) {
   let step = indx < indx2 ? 1 : -1;
   for (let i = indx; i !== indx2 + step; i += step) {
-    checkboxes[i].checked = true;
+    checkboxes[i].checked = true; //we take starting index from last checked and final index from input and then check all in between
   }
 }
